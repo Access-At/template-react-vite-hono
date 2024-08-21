45525b2c8b7c2ea573896ae15608a08370942074
Farhan Reizha <48412599+farhanreizha@users.noreply.github.com>
2024-08-22 06:00:28 +0700

    feat: api route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,34 +1,12 @@
+import { example } from "@/api/routes/example";
 import { Hono } from "hono";
-import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 
 const app = new Hono().basePath("/api");
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-const route = app
-  .get("/", (c) =>
-    c.json({
-      status: true,
-      message: "Hello this is api",
-    })
-  )
-  .get("/test", (c) =>
-    c.json(
-      {
-        ok: true,
-        message: "Successfully get data",
-        data: {
-          title: "Title Hello World",
-          desc: "Description Hello World",
-        },
-      },
-      200
-    )
-  );
+app.use(logger());
+
+const route = app.route("/", example);
 
 export type ServerType = typeof route;
 
